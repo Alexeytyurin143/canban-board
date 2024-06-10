250ef9f41567cdcca3d46ad70cb0ef832173bc5e
Alexey Tyurin <alexsei89tyrin@gmail.com>
2024-06-10 18:03:51 +0400

    import fix

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,5 +1,3 @@
-import { create } from '@/actions/createBoard/create-board'
-import { Button } from '@/components/ui/button'
 import { db } from '@/lib/db'
 import { Board } from './_components/Board'
 import { Form } from './_components/Form'
